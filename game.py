2f9e3e0a146e044df923b8fbc4eba2394d8acad9
Pavle Djurakic <djurakicpavle@gmail.com>
2026-05-30 22:23:37 +0200

    Implementirane klase Game i GameState, kao i nadogradjenja metoda za generisanje pokreta.

---
+++ game.py
@@ -0,0 +1,143 @@
+from board import Board
+from square import Square
+from piece import Piece, Player, PieceType
+import move_generator as mg
+import copy
+
+class Move:
+    """
+    Svaki potez je objekat ove klase i posle se koristi za heuristiku, itd.
+    """
+    from_row: int
+    from_col: int
+    to_row: int
+    to_col: int
+    captured: list
+    
+    def __init__(self, from_row, from_col, to_row, to_col, captured = None):
+        self.from_row = from_row
+        self.from_col = from_col
+        self.to_row = to_row
+        self.to_col = to_col
+        self.captured = captured if captured is not None else []
+
+
+
+class Game:
+    draw_counter: int
+    board: Board
+    current_player: Player
+    winner: Player
+    # undo_stack: Stack
+
+    def __init__(self):
+        self.draw_counter = 0
+        self.board = Board()
+        self.current_player = Player.BELI
+        self.winner = None
+
+
+    def apply_move(self, move: Move, board: Board):
+        if self.is_valid_move(move, board): # type: ignore
+
+            piece = board.get_piece(move.from_row, move.from_col)
+            board.get_square(move.from_row, move.from_col).piece = None
+            target = board.get_square(move.to_row, move.to_col)
+            target.piece = piece
+
+            for (captured_row, captured_col) in move.captured:
+                board.get_square(captured_row, captured_col).piece = None
+
+            type_before = piece.piece_type
+            board.check_promotion(target)
+            type_after = piece.piece_type
+
+            promoted = (type_before != type_after)
+
+            if move.captured or promoted:
+                self.draw_counter = 0
+            else:
+                self.draw_counter += 1
+
+            self.current_player = Player.CRNI if self.current_player == Player.BELI else Player.BELI
+
+        else:
+            raise Exception("Ovo nije validan potez! Pokusajte ponovo!")
+
+
+    def get_all_moves(self, player: Player, board: Board) -> list:
+        """
+        Vraca sve validne poteze za sve figure zadatog igraca.
+
+        Jedenje je globalno obavezno — ako IJEDNA figura moze da skace,
+        normalni potezi svih figura se odbacuju.
+
+        Vraca listu Move objekata.
+        """
+        normal_moves = []
+        jump_moves   = []
+
+        for (piece, square) in board.get_all_pieces(player):
+            for t in mg.get_moves(square, board):
+                move = Move(
+                    from_row  = square.row,
+                    from_col  = square.col,
+                    to_row    = t[0],
+                    to_col    = t[1],
+                    captured  = t[2]
+                )
+                if move.captured:
+                    jump_moves.append(move)
+                else:
+                    normal_moves.append(move)
+
+        return jump_moves if jump_moves else normal_moves
+
+
+    def is_game_over(self, board: Board) -> bool:
+        if board.get_all_pieces(Player.BELI)==[] or board.get_all_pieces(Player.CRNI)==[]:
+            return True
+        elif self.draw_counter >= 40:
+            return True
+        else:
+            return False
+
+
+    def get_winner(self, board: Board):
+        if board.get_all_pieces(Player.BELI)==[]:
+            return Player.CRNI
+        elif board.get_all_pieces(Player.CRNI)==[]:
+            return Player.BELI
+        elif self.draw_counter >= 40:
+            return None
+        else:
+            raise Exception("Do pobede nije doslo!")
+
+
+    def is_valid_move(self, move: Move, board: Board):
+        if not 0<=move.to_row<=7 or not 0<=move.to_col<=7:
+            return False
+        elif not 0<=move.from_row<=7 or not 0<=move.from_col<=7:
+            return False
+        elif not board.get_square(move.to_row, move.to_col).is_usable:
+            return False
+        else:
+            return True
+
+
+class GameState:
+    """
+    Zamrznto trenutno stanje Game-a pre odigravanja sledeceg poteza.
+    Koristi se za undo stack.
+    """
+    draw_counter: int
+    board: Board
+    current_player: Player
+
+    def __init__(self, board, draw_counter, current_player):
+        self.draw_counter = draw_counter
+        self.board = copy.deepcopy(board)
+        self.current_player = current_player
+
+
+        
--- move_generator.py
+++ move_generator.py
@@ -153,15 +153,82 @@ def get_kraljevic_moves(square: Square, board: Board) -> list:
             target_row = square.row + row_direction
             target_col = square.col + col_direction
             
-            if is_out_of_bounds(target_row, target_col):
+            while not is_out_of_bounds(target_row, target_col):
+                piece_on_target = board.get_piece(target_row, target_col)
+                
+                if piece_on_target is None:
+                    normal_moves.append((target_row, target_col, []))
+
+                elif piece_on_target.player != player:
+                    landing_row = target_row + row_direction
+                    landing_col = target_col + col_direction
+
+                    if is_out_of_bounds(landing_row, landing_col):
+                        break
+                    if board.get_piece(landing_row, landing_col) is not None:
+                        break
+
+                    already_captured = {(target_row, target_col)}
+
+                    chains = get_kraljevic_jump_chains(landing_row, landing_col, board, player, row_direction, col_direction, already_captured)
+
+                    for (final_row, final_col, more_caps) in chains:
+                        jump_moves.append( (final_row, final_col, [(target_row, target_col)] + more_caps) )
+                    
+                    break
+                
+                target_row += row_direction
+                target_col += col_direction
+
+    # Jedenje je obavezno — ako ima skokova, normalni potezi se ignorisu 
+    return jump_moves if jump_moves else normal_moves
+
+
+def get_kraljevic_jump_chains(landing_row, landing_col, board, player, came_from_row_dir, came_from_col_dir, already_captured):
+    """
+    Rekurzivna funkcija. Poziva se sa landing pozicije nakon skoka kraljevica.
+    Trazi sve moguce dalje lance skokova od te pozicije u svim pravcima osim odakle je dosao, da ne pojede 2 puta istog.
+
+    Vraca listu (final_row, final_col, [dalje_pojedeni]) za svaki moguci put.
+    Ako nema daljeg skakanja, vraca [(landing_row, landing_col, [])].
+
+    already_captured: skup vec pojedenih figura u ovom lancu (ne smemo preskociti isti komad dva puta)
+    """
+    paths = []
+    
+    for row_dir in (+1, -1):
+        for col_dir in (+1, -1):
+            if row_dir == -came_from_row_dir and col_dir == -came_from_col_dir:
+                continue
+            
+            enemy_row = landing_row + row_dir
+            enemy_col = landing_col + col_dir
+            next_landing_row = landing_row + 2 * row_dir
+            next_landing_col = landing_col + 2 * col_dir
+
+            if is_out_of_bounds(enemy_row, enemy_col):
+                continue
+            if is_out_of_bounds(next_landing_row, next_landing_col):
                 continue
 
-            piece_on_target = board.get_piece(target_row, target_col)
-            
-            if piece_on_target is None:
-                normal_moves.append((target_row, target_col, []))
+            enemy = board.get_piece(enemy_row, enemy_col)
+            next_landing = board.get_piece(next_landing_row, next_landing_col)
 
-            elif piece_on_target.player != player:
+            enemy_is_capturable = (
+                enemy is not None
+                and enemy.player != player
+                and next_landing is None
+                and (enemy_row, enemy_col) not in already_captured
+            )
 
+            if enemy_is_capturable:
+                new_captured = already_captured | {(enemy_row, enemy_col)}
+                sub_paths = get_kraljevic_jump_chains(next_landing_row, next_landing_col, board, player, row_dir, col_dir, new_captured)
+                for (final_row, final_col, more_caps) in sub_paths:
+                    paths.append( (final_row, final_col, [(enemy_row, enemy_col)] + more_caps) )
 
-    pass
+    # Nema vise skokova - ova landing pozicija je konacna
+    if not paths:
+        return [(landing_row, landing_col, [])]
+
+    return paths
